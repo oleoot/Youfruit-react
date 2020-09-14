17f3e3604e494c2532e55abdcc8f59941633e8cd
oleoot <oleoot28@gmail.com>
2020-09-14 20:53:51 +0300

    Style cleanup for blog page

--- youfruit-react/src/pages/about/about.js
+++ youfruit-react/src/pages/about/about.js
@@ -30,16 +30,6 @@ class About extends Component {
         map.addControl(new MapboxLanguage({
             defaultLanguage: 'ru'
         }));
-        // const marker = new mapboxgl.Marker()
-        //     .setLngLat([30.616444, 50.401744])
-        //     .addTo(map);
-        // const geocoder = new MapboxGeocoder({
-        //     accessToken: mapboxgl.accessToken,
-        //     language: 'ru-RU',
-        //     mapboxgl: mapboxgl
-        // });
-        // map.addControl(geocoder);
-
         const geojson = [
             {
                 type: 'Feature',
@@ -59,45 +49,42 @@ class About extends Component {
         })
     }
 
-
-
     render() {
         return (
+            <div className="about">
+                <section className="info">
+                    <div className="container-xs">
+                        <div className="breadcrumbs-btn">
+                            <NavLink exact to="/" activeClassName="nav-active" className="breadcrumbs-btn__link text_lg font_regular">
+                                <img src={require(`../../img/icons/arrow-left.png`)} alt="arrow-left" className="breadcrumbs-btn__img" />Главная</NavLink>
+                        </div>
+                        <div className="section-headline">
+                            <div className="section-headline_underline"></div>
+                            <div className="section-headline__textWrap">
+                                <p className="section-headline_text text_lg">О нас</p>
+                            </div>
+                            <div className="section-headline_underline"></div>
+                        </div>
+                        <div className="info__innerWrap">
+                            <div className="info__item">
+                                <img src={require(`../../img/about/logo.png`)} alt="muffins" className="info__img" />
+                                <p className="info__description text_sm font_regular">Есть над чем задуматься: предприниматели в сети интернет будут заблокированы в рамках своих собственных рациональных ограничений. С учётом сложившейся международной обстановки, высококачественный прототип будущего проекта предоставляет широкие возможности для глубокомысленных рассуждений.</p>
+                            </div>
 
-            <section className="about">
-                <div className="container-xs">
-                    <div className="back-btn">
-                        <NavLink exact to="/" activeClassName="nav-active" className="text_lg breadcrumbs-link">
-                            <img src={require(`../../img/icons/arrow-left.png`)} alt="arrow-left" className="arrow-left" />Главная</NavLink>
-                    </div>
-                    <div className="section-headline">
-                        <div className="section-headline_underline"></div>
-                        <div className="section-headline__textWrap">
-                            <p className="section-headline_text text_lg">О нас</p>
-                        </div>
-                        <div className="section-headline_underline"></div>
-                    </div>
-                </div>
-                <div className="container-xs">
-                    <div className="about-wrap">
-                        <div className="about-item" id="about-company">
-                            <img src={require(`../../img/about/logo.png`)} alt="muffins" className="about-logo-img" />
-                            <p className="text_sm  about__description about-logo-text">Есть над чем задуматься: предприниматели в сети интернет будут заблокированы в рамках своих собственных рациональных ограничений. С учётом сложившейся международной обстановки, высококачественный прототип будущего проекта предоставляет широкие возможности для глубокомысленных рассуждений.</p>
-                        </div>
-
-                        <div className="about-item">
-                            <img src={require(`../../img/about/daria.png`)} alt="muffins" />
-                            <p className="text_lg about__name">Дарья</p>
-                            <p className="text_sm about__description ">Как уже неоднократно упомянуто, акционеры крупнейших компаний, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут объединены в целые кластеры себе подобных.</p>
-                        </div>
-                        <div className="about-item">
-                            <img src={require(`../../img/about/oleg.png`)} alt="muffins" />
-                            <p className="text_lg about__name">Олег</p>
-                            <p className="text_sm about__description ">С учётом сложившейся международной обстановки, сплочённость команды профессионалов напрямую зависит от системы массового участия.</p>
+                            <div className="info__item">
+                                <img src={require(`../../img/about/daria.png`)} alt="muffins" className="info__img" />
+                                <p className="info__name text_lg font_semibold">Дарья</p>
+                                <p className="info__description text_sm font_regular">Как уже неоднократно упомянуто, акционеры крупнейших компаний, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут объединены в целые кластеры себе подобных.</p>
+                            </div>
+                            <div className="info__item">
+                                <img src={require(`../../img/about/oleg.png`)} alt="muffins" className="info__img" />
+                                <p className="info__name text_lg font_semibold">Олег</p>
+                                <p className="info__description text_sm font_regular">С учётом сложившейся международной обстановки, сплочённость команды профессионалов напрямую зависит от системы массового участия.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <section className="about__info">
+                </section>
+                <section className="contact">
                     <div className="container-xs">
                         <div className="section-headline">
                             <div className="section-headline_underline"></div>
@@ -107,32 +94,32 @@ class About extends Component {
                             <div className="section-headline_underline"></div>
                         </div>
 
-                        <div className="about__info_inner">
-                            <div className="about__info_phone">
+                        <div className="contact__innerWrap">
+                            <div className="contact__infoPane">
                                 <ul>
-                                    <li>youfruit.shop@gmail.com</li>
-                                    <li>+380667395800</li>
-                                    <li>+380667395800</li>
-                                    <li>м.Осокорки</li>
-                                    <li>Княжий Затон 14Б</li>
+                                    <li className="contact__listItem">youfruit.shop@gmail.com</li>
+                                    <li className="contact__listItem">+380667395800</li>
+                                    <li className="contact__listItem">+380667395800</li>
+                                    <li className="contact__listItem">м.Осокорки</li>
+                                    <li className="contact__listItem">Княжий Затон 14Б</li>
                                 </ul>
                             </div>
-                            <div className="about__info_call">
-                                <p className="about__info_headline">Свяжитесь с нами</p>
-                                <form>
-                                    <div className="form__top">
+                            <div className="contact__formPane">
+                                <p className="contact__headline">Свяжитесь с нами</p>
+                                <form className="contact-form">
+                                    <div className="contact-form__top">
                                         <div>
-                                            <label htmlFor="" >Имя</label>
-                                            <input type="text" placeholder="Имя..." />
+                                            <label htmlFor="" className="contact-form__label">Имя</label>
+                                            <input type="text" placeholder="Имя..." className="contact-form__input" />
                                         </div>
                                         <div>
-                                            <label htmlFor="" >Телефон</label>
-                                            <input type="text" placeholder="Телефон..." />
+                                            <label htmlFor="" className="contact-form__label">Телефон</label>
+                                            <input type="text" placeholder="Телефон..." className="contact-form__input" />
                                         </div>
                                     </div>
-                                    <div className="form__bottom">
-                                        <label htmlFor="">Сообщение</label>
-                                        <textarea placeholder="Сообщение..."></textarea>
+                                    <div className="contact-form__bottom">
+                                        <label htmlFor="" className="contact-form__label">Сообщение</label>
+                                        <textarea placeholder="Сообщение..." className="contact-form__textarea"></textarea>
                                     </div>
                                     <button className="btn">Отправить сообщение</button>
                                 </form>
@@ -141,7 +128,7 @@ class About extends Component {
                     </div>
                 </section>
                 <section className="map">
-                    <div className="container-xs">
+                    <div className="container">
                         <div className="section-headline">
                             <div className="section-headline_underline"></div>
                             <div className="section-headline__textWrap">
@@ -151,16 +138,19 @@ class About extends Component {
                         </div>
                     </div>
                     <div className="mapContainer__wrap">
+                        <div className="mapContainer__info">
+                            <ul>
+                                <li className="mapContainer__listItem text_sm font_semibold">youfruit.shop@gmail.com</li>
+                                <li className="mapContainer__listItem text_sm font_semibold">+380667395800</li>
+                                <li className="mapContainer__listItem text_sm font_semibold">м.Осокорки</li>
+                                <li className="mapContainer__listItem text_sm font_semibold">Княжий Затон 14Б</li>
+
+                            </ul>
+                        </div>
                         <div ref={el => this.mapContainer = el} className="mapContainer" />
                     </div>
                 </section>
-
-            </section>
-
-
-
-
-
+            </div>
         )
     }
 }
--- youfruit-react/src/pages/blog/blog.js
+++ youfruit-react/src/pages/blog/blog.js
@@ -15,32 +15,31 @@ class Blog extends Component {
     }
     render() {
         return (
-
-            <section className="blog">
-                <div className="container">
-                    <div className="back-btn">
-                        <NavLink exact to="/" activeClassName="nav-active" className="text_lg breadcrumbs-link">
-                            <img src={require(`../../img/icons/arrow-left.png`)} alt="arrow-left" className="arrow-left" />Главная</NavLink>
-                    </div>
-                    <div className="blog-wrap grid-container">
-                        {this.state.blogPosts.map((post) => (
-                            <div className="grid-container blog-item">
-
-                                <img src={post.img} alt="porrige" className="main-img" />
-                                <div>
-                                    <p className="text_lg blog-post__name">{post.name}</p>
-                                    <p className="text_md blog-post__description">{post.description}</p>
-                                    <Link to={post.link} className="btn text_sm" >Читать</Link>
-                                </div>
+            <div className="blog-page">
+                <section className="blog">
+                    <div className="container">
+                        <div className="container">
+                            <div className="breadcrumbs-btn">
+                                <NavLink exact to="/" activeClassName="nav-active" className="breadcrumbs-btn__link text_lg font_regular">
+                                    <img src={require(`../../img/icons/arrow-left.png`)} alt="arrow-left" className="breadcrumbs-btn__img" />Главная</NavLink>
                             </div>
-                        ))}
+                        </div>
+                        <div className="blog__innerWrap">
+                            {this.state.blogPosts.map((post) => (
+                                <div className="blog__item">
+                                    <img src={post.img} alt="porrige" className="blog__img" />
+                                    <div>
+                                        <p className="blog__name text_lg font_semibold">{post.name}</p>
+                                        <p className="blog__description text_md font_regular">{post.description}</p>
+                                        <Link to={post.link} className="btn text_sm" >Читать</Link>
+                                    </div>
+                                </div>
+                            ))}
 
+                        </div>
                     </div>
-
-
-                </div>
-
-            </section>
+                </section>
+            </div>
 
 
 
