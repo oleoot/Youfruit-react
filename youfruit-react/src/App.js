a38732cb879a66abce3e7def1f81a3bfd4a4cce9
oleoot <oleoot28@gmail.com>
2020-09-14 13:33:10 +0300

    Style cleanup for footer

--- youfruit-react/src/App.js
+++ youfruit-react/src/App.js
@@ -141,22 +141,9 @@ class App extends Component {
               </div>
             </div>
           </header>
-          <div className="cart">
-            <div className="cart-header grid-container align-center">
-              <div className="flex-container cart-cross">
-                <div className="cart-line" id="line1"></div>
-                <div className="cart-line" id="line2"></div>
-              </div>
-              <div>
-                <p className="text-md">Корзина</p>
-              </div>
-              <div></div>
-            </div>
-          </div>
           <Switch>
             <Route exact path="/" component={Main}>
             </Route>
-
             <Route exact path="/shop" component={Shop}>
             </Route>
             {this.state.products.map(product => (
@@ -181,36 +168,23 @@ class App extends Component {
 
             <Route path="/cart" render={(props) => <Cart {...props} cartInside={this.state.cartInside} removeFromCartProduct={this.removeFromCartProduct} removeFromCartNumber={this.removeFromCartNumber} cartTotal={this.state.cartTotal} removeFromTotal={this.removeFromTotal} />} >
             </Route>
-
-            {/* <Route path="/blog/pancakes" component={Post}></Route> */}
-
-
           </Switch>
-
-          <footer id="footer" className="footer">
-
+          <footer className="footer">
             <div className="container">
               <div className="footer__wrap">
                 <div className="footer__left">
                   <div className="footer__numberWrap">
-                    <img src={require(`./img/icons/lifecell.png`)} alt="lifecell" />
-                    <a href="tel:+380667338900" className="text_sm footer__number">+380674591254</a>
+                    <img src={require(`./img/icons/lifecell.png`)} alt="lifecell" className="footer__providerImg" />
+                    <a href="tel:+380667338900" className="footer__number text_sm">+380674591254</a>
                   </div>
                   <div className="footer__numberWrap flex-container align-center footer-call-margin">
-                    <img src={require(`./img/icons/vodafone.png`)} alt="vodafone" />
-                    <a href="tel:+380508373513" className="text_sm footer__number">+380508373513</a>
+                    <img src={require(`./img/icons/vodafone.png`)} alt="vodafone" className="footer__providerImg" />
+                    <a href="tel:+380508373513" className="footer__number text_sm">+380508373513</a>
                   </div>
                 </div>
-
                 <div className="footer__middle">
-                  <p className="text_xs footer__copy">&copy; Youfruit 2020</p>
-                  {/* <input type="tel" placeholder="Ваш телефон" className="footer__input" />
-                  <button type="submit" className="footer__button text_xs">Задать вопрос</button> */}
-
+                  <p className="footer__copy text_xs">&copy; Youfruit 2020</p>
                 </div>
-
-
-
                 <div className="footer__right">
                   <a href="https://www.instagram.com/youfruit_shop/" target="_blank" className="footer__right_imgWrap">
                     <svg viewBox="0 0 512.00096 512.00096" width="30" height="30" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path d="m373.40625 0h-234.8125c-76.421875 0-138.59375 62.171875-138.59375 138.59375v234.816406c0 76.417969 62.171875 138.589844 138.59375 138.589844h234.816406c76.417969 0 138.589844-62.171875 138.589844-138.589844v-234.816406c0-76.421875-62.171875-138.59375-138.59375-138.59375zm108.578125 373.410156c0 59.867188-48.707031 108.574219-108.578125 108.574219h-234.8125c-59.871094 0-108.578125-48.707031-108.578125-108.574219v-234.816406c0-59.871094 48.707031-108.578125 108.578125-108.578125h234.816406c59.867188 0 108.574219 48.707031 108.574219 108.578125zm0 0" /><path d="m256 116.003906c-77.195312 0-139.996094 62.800782-139.996094 139.996094s62.800782 139.996094 139.996094 139.996094 139.996094-62.800782 139.996094-139.996094-62.800782-139.996094-139.996094-139.996094zm0 249.976563c-60.640625 0-109.980469-49.335938-109.980469-109.980469 0-60.640625 49.339844-109.980469 109.980469-109.980469 60.644531 0 109.980469 49.339844 109.980469 109.980469 0 60.644531-49.335938 109.980469-109.980469 109.980469zm0 0" /><path d="m399.34375 66.285156c-22.8125 0-41.367188 18.558594-41.367188 41.367188 0 22.8125 18.554688 41.371094 41.367188 41.371094s41.371094-18.558594 41.371094-41.371094-18.558594-41.367188-41.371094-41.367188zm0 52.71875c-6.257812 0-11.351562-5.09375-11.351562-11.351562 0-6.261719 5.09375-11.351563 11.351562-11.351563 6.261719 0 11.355469 5.089844 11.355469 11.351563 0 6.257812-5.09375 11.351562-11.355469 11.351562zm0 0" /></svg>
@@ -226,9 +200,6 @@ class App extends Component {
                   </a>
                 </div>
               </div>
-
-
-
             </div>
           </footer>
         </div>
