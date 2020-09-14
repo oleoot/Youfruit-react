3f17bc3744a7ed84f8799fb0b3e0f0a568e0fb16
oleoot <oleoot28@gmail.com>
2020-09-14 22:24:42 +0300

    Style cleanup for shopItem component

--- youfruit-react/src/product-pages/shopItem.js
+++ youfruit-react/src/product-pages/shopItem.js
@@ -10,17 +10,17 @@ class ShopItem extends Component {
     render() {
         const { product } = this.props;
         return (
-            <Link to={product.link} className="shop-item" >
-                <div className="shop-img-wrap">
-                    <img src={product.img} alt="logo" className="main-img" />
-                    <div className="shop-weight flex-container-center align-center">
-                        <p className="text_sm shop__weight">{product.weight}</p>
+            <Link to={product.link} className="shop__item" >
+                <div className="shop__imgWrap">
+                    <img src={product.img} alt="logo" className="shop__productImg" />
+                    <div className="shop__productWeight font_semibold">
+                        <p className="text_sm">{product.weight}</p>
                     </div>
                 </div>
-                <div className="item-wrap">
+                <div className="shop__textWrap">
                     <div>
-                        <p className="text_md shop-item-name">{product.name}</p>
-                        <p className="text_md shop-item-price">{product.price} грн.</p>
+                        <p className="shop__name text_md font_regular">{product.name}</p>
+                        <p className="shop__price text_md font_semibold">{product.price} грн.</p>
                     </div>
                     <div className="item-btn">
                         <Link to={product.link} className="btn text_xs">Купить</Link>
